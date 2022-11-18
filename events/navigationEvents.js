2b387fdaff50c17ef974cd7663254c0483d9eeb6
Erin Stephens <erebstephens@gmail.com>
2022-11-17 20:54:23 -0600

    attempted search

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -37,10 +37,10 @@ const navigationEvents = () => {
   document.querySelector('#search').addEventListener('keyup', (e) => {
     const searchValue = document.querySelector('#search').value.toLowerCase();
     console.warn(searchValue);
-
     // WHEN THE USER PRESSES ENTER, MAKE THE API CALL AND CLEAR THE INPUT
     if (e.keyCode === 13) {
       // MAKE A CALL TO THE API TO FILTER ON THE BOOKS
+      showBooks(searchValue);
       // IF THE SEARCH DOESN'T RETURN ANYTHING, SHOW THE EMPTY STORE
       // OTHERWISE SHOW THE STORE
 
